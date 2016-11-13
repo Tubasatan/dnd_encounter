50f647899774a66dbec45658ab5b8f2d886ab6dc
Sebastian Antonsen <sebastian@tubatools.com>
2016-11-13 10:38:25 +0100

    changed README to .rest / added .pypirc for uploading to PyPI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 
 setuptools.setup(name='dnd_encounter',
-                 version='0.4.1',
+                 version='0.4.2',
                  descripion='Tool to fill out a pdf with monster data',
                  url='https://github.com/Tubasatan/dnd_encounter',
                  author='Sebastian Antonsen',
